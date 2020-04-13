7925374ab0d67579248e9ac3eb125ce4427d0702
meherett <meherett@zoho.com>
2020-04-13 15:02:07 +0300

    Upgrade v0.1.0.dev1 to v0.1.0

--- eth_wallet/__init__.py
+++ eth_wallet/__init__.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+from .wallet import Wallet
+from .utils import utils, base58
+
+# ETH-Wallet Information's
+__version__ = "0.1.0"
+__license__ = "GPL-3.0"
+__author__ = "Meheret Tesfaye"
+__email__ = "meherett@zoho.com"
+
+
+__all__ = [
+    "__version__",
+    "__license__",
+    "__author__",
+    "__email__",
+    "Wallet",
+    "utils",
+    "base58"
+]
