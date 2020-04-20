1c36a9fd32c3e022541fd89f7b2a14da84ba7c99
meherett <meherett@zoho.com>
2020-04-20 13:12:40 +0300

    Update eth_wallet

--- eth_wallet/__init__.py
+++ eth_wallet/__init__.py
@@ -1,25 +1,8 @@
 #!/usr/bin/env python3
 
-
-# Information's
-__version__ = "0.2.6"
-__license__ = "GPL-3.0"
-__author__ = "Meheret Tesfaye"
-__email__ = "meherett@zoho.com"
-
-
-# Wallet class
 from .wallet import Wallet
-
-modules = [
-    "Wallet"
-]
 
 
 __all__ = [
-    "__version__",
-    "__license__",
-    "__author__",
-    "__email__",
-    *modules
+    "Wallet"
 ]
