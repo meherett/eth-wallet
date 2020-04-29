ee467c04cc77198fe6853792822bab05e669de36
meherett <meherett@zoho.com>
2020-04-29 20:19:37 +0300

    Upgrade examples

--- examples/from_entropy.py
+++ examples/from_entropy.py
@@ -31,45 +31,23 @@ wallet.from_index(0, harden=True)
 # Print all wallet information's
 # print(json.dumps(wallet.dumps(), indent=4, ensure_ascii=False))
 
-# Get Entropy
 print("Entropy:", wallet.entropy())
-# Get Mnemonic
 print("Mnemonic:", wallet.mnemonic())
-# Get Language
 print("Language:", wallet.language())
-# Get Passphrase
 print("Passphrase:", wallet.passphrase())
-# Get Seed
 print("Seed:", wallet.seed())
-# Get Private Key
 print("Private Key:", wallet.private_key())
-# Get Public Key
 print("Public Key:", wallet.public_key())
-# Get Uncompressed Public Key
 print("Uncompressed:", wallet.uncompressed())
-# Get Wallet Import Format
 print("Wallet Import Format:", wallet.wallet_import_format())
-# Get Finger Print
 print("Finger Print:", wallet.finger_print())
-# Get Chain Code
 print("Chain Code:", wallet.chain_code())
-# Get Derivation Path
-print("Derivation Path:", wallet.path())
-# Get Address
+print("Path:", wallet.path())
 print("Address:", wallet.address())
 
-# ---- Serialized ----
 print("---- Serialized ----")
 
-# Get Private Key Hex
-print("Private Key Hex:",
-      wallet.extended_key(private_key=True, encoded=False))
-# Get Public Key Hex
-print("Public Key Hex:",
-      wallet.extended_key(private_key=False, encoded=False))
-# Get Private Key Base58
-print("Private Key Base58:",
-      wallet.extended_key(private_key=True, encoded=True))
-# Get XPublic Key Base58
-print("Public Key Base58:",
-      wallet.extended_key(private_key=False, encoded=True))
+print("Private Key Hex:", wallet.extended_key(private_key=True, encoded=False))
+print("Public Key Hex:", wallet.extended_key(private_key=False, encoded=False))
+print("Private Key Base58:", wallet.extended_key(private_key=True, encoded=True))
+print("Public Key Base58:", wallet.extended_key(private_key=False, encoded=True))
--- examples/from_private_key.py
+++ examples/from_private_key.py
@@ -15,15 +15,9 @@ wallet.from_private_key(private_key=PRIVATE_KEY)
 # Print all wallet information's
 # print(json.dumps(wallet.dumps(), indent=4, ensure_ascii=False))
 
-# Get Private Key
 print("Private Key:", wallet.private_key())
-# Get Public Key
 print("Public Key:", wallet.public_key())
-# Get Uncompressed Public Key
 print("Uncompressed:", wallet.uncompressed())
-# Get Wallet Import Format
 print("Wallet Import Format:", wallet.wallet_import_format())
-# Get Finger Print
 print("Finger Print:", wallet.finger_print())
-# Get Address
 print("Address:", wallet.address())
