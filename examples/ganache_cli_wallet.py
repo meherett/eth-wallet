78c14502f9e0bf0478b66fa3bb1e1f8ab1eae2ad
meherett <meherett@zoho.com>
2020-05-07 21:17:31 +0300

    Added ganache_cli_wallet.py example.

---
+++ examples/ganache_cli_wallet.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from eth_wallet import Wallet
+from eth_wallet.utils import generate_entropy
+
+# 128 strength entropy
+ENTROPY = generate_entropy(strength=128)
+# Secret passphrase
+PASSPHRASE = None  # str("meherett")
+# Choose language english, french, italian, spanish, chinese_simplified, chinese_traditional, japanese & korean
+LANGUAGE = "japanese"  # default is english
+
+# Initialize wallet
+wallet = Wallet()
+# Get Ethereum wallet from entropy
+wallet.from_entropy(entropy=ENTROPY, passphrase=PASSPHRASE, language=LANGUAGE)
+
+print("Mnemonic:", wallet.mnemonic())
+print("Base HD Path:  m/44'/60'/0'/0/{account_index}", "\n")
+
+# Print account index, address and private key wallet information's
+for account_index in range(10):
+    # Derivation from path
+    # wallet.from_path(f"m/44'/60'/0'/0/{account_index}")
+    # Derivation from index
+    wallet.from_index(44, harden=True)
+    wallet.from_index(60, harden=True)
+    wallet.from_index(0, harden=True)
+    wallet.from_index(0)
+    wallet.from_index(account_index)
+    # Print account_index, address and private_key like ganache-cli
+    print(f"({account_index}) {wallet.path()} {wallet.address()} 0x{wallet.private_key()}")
+    # Clear derivation
+    wallet.clear_derivation()
